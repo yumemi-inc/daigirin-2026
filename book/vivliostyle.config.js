29563f02a6018d9df4712156dd17f32d97b3bdc3
Masa1984a <april.macchiato@gmail.com>
2026-02-18 01:02:45 +0900

    solana swap initial

--- book/vivliostyle.config.js
+++ book/vivliostyle.config.js
@@ -16,6 +16,7 @@ module.exports = {
     // 各章の原稿
     'sample_chapter.md', // サンプル用ページです。執筆時は削除してください。
     'solana-nft-complete-guide.md',
+    'solana-swap-complete-guide.md',
     // 著者紹介
     'authors.md',
     // 奥付
